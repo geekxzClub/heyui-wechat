9a5e63fa634e643612447972c6a42e89f0cc5390
geekxz <13298176895@163.com>
2021-03-11 17:24:31 +0800

    新增扩展

--- examples/pages/plugin/plugin.js
+++ examples/pages/plugin/plugin.js
@@ -5,7 +5,87 @@ Page({
    * 页面的初始数据
    */
   data: {
+    tplList: [{
+      id: 1,
+      icon: 'cardboardfill',
+      type: 'actionsheet',
+      text_zh: '电商购物',
+      text_en: 'Hey-Shop'
+    }, {
+        id: 2,
+        icon: 'cardboardfill',
+        type: 'toast',
+        text_zh: '抽奖',
+        text_en: 'Hey-Prize'
+    }, {
+      id: 3,
+      icon: 'cardboardfill',
+      type: 'message',
+      text_zh: '预约会客',
+      text_en: 'Hey-Meeting'
+    }, {
+      id: 4,
+      icon: 'cardboardfill',
+      type: 'dialog',
+      text_zh: '课程表',
+      text_en: 'Hey-Timetable'
+    }, {
+      id: 5,
+      icon: 'cardboardfill',
+      type: 'dialog',
+      text_zh: '个人博客',
+      text_en: 'Hey-Blog'
+    }, {
+      id: 6,
+      icon: 'cardboardfill',
+      type: 'dialog',
+      text_zh: '在线聊天',
+      text_en: 'Hey-Chat'
+    }, {
+      id: 7,
+      icon: 'cardboardfill',
+      type: 'dialog',
+      text_zh: '内容管理系统',
+      text_en: 'Hey-Cms'
+    }, {
+      id: 8,
+      icon: 'cardboardfill',
+      type: 'dialog',
+      text_zh: '客户管理系统',
+      text_en: 'Hey-Crm'
+    }, {
+      id: 8,
+      icon: 'cardboardfill',
+      type: 'dialog',
+      text_zh: '问卷调查',
+      text_en: 'Hey-Thorough'
+    }, {
+      id: 8,
+      icon: 'cardboardfill',
+      type: 'dialog',
+      text_zh: '企业官网',
+      text_en: 'Hey-Leading'
+    }],
 
+    funList: [{
+      id: 1,
+      icon: 'cardboardfill',
+      type: 'actionsheet',
+      text_zh: '数组',
+      text_en: 'H-Array'
+    }, {
+        id: 2,
+        icon: 'cardboardfill',
+        type: 'toast',
+        text_zh: '字符串',
+        text_en: 'H-String'
+    }, {
+      id: 3,
+      icon: 'cardboardfill',
+      type: 'message',
+      text_zh: '判断',
+      text_en: 'H-Is'
+    }]
   },
 
   /**
